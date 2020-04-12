20fd2f08f6319af4a1a97c69a7352280b43c35cc
Adam Bajguz <adam.bajguz@gmail.com>
2020-04-12 17:21:49 +0200

    Migrations

--- backend/TrainsOnline.Domain/Entities/User.cs
+++ backend/TrainsOnline.Domain/Entities/User.cs
@@ -14,11 +14,18 @@
         public DateTime LastSavedOn { get; set; }
         public Guid? LastSavedBy { get; set; }
 
-        [AuditIgnore]
-        public string Password { get; set; } = default!;
 
         public string Username { get; set; } = default!;
         public string Email { get; set; } = default!;
+
+        [AuditIgnore]
+        public string Password { get; set; } = default!;
+
+        public string Name { get; set; } = default!;
+        public string Surname { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+        public string Address { get; set; } = default!;
+
         public bool IsAdmin { get; set; }
 
         public ICollection<Ticket> Tickets { get; set; } = default!;
---
+++ backend/TrainsOnline.Persistence/Migrations/20200412153836_Initial.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TrainsOnline.Persistence.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EntityAuditLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: true),
+                    TableName = table.Column<string>(nullable: false),
+                    Key = table.Column<Guid>(nullable: false),
+                    Action = table.Column<int>(nullable: false),
+                    Values = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntityAuditLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: true),
+                    LastSavedOn = table.Column<DateTime>(nullable: false),
+                    LastSavedBy = table.Column<Guid>(nullable: true),
+                    Name = table.Column<string>(nullable: false),
+                    Latitude = table.Column<double>(nullable: false),
+                    Longitude = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: true),
+                    LastSavedOn = table.Column<DateTime>(nullable: false),
+                    LastSavedBy = table.Column<Guid>(nullable: true),
+                    Username = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Surname = table.Column<string>(nullable: false),
+                    PhoneNumber = table.Column<string>(nullable: false),
+                    Address = table.Column<string>(nullable: false),
+                    IsAdmin = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Routes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: true),
+                    LastSavedOn = table.Column<DateTime>(nullable: false),
+                    LastSavedBy = table.Column<Guid>(nullable: true),
+                    FromId = table.Column<Guid>(nullable: false),
+                    ToId = table.Column<Guid>(nullable: false),
+                    DepartureTime = table.Column<DateTime>(nullable: false),
+                    Duration = table.Column<TimeSpan>(nullable: false),
+                    Distance = table.Column<double>(nullable: false),
+                    TicketPrice = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Routes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Routes_Stations_FromId",
+                        column: x => x.FromId,
+                        principalTable: "Stations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Routes_Stations_ToId",
+                        column: x => x.ToId,
+                        principalTable: "Stations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tickets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedOn = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<Guid>(nullable: true),
+                    LastSavedOn = table.Column<DateTime>(nullable: false),
+                    LastSavedBy = table.Column<Guid>(nullable: true),
+                    UserId = table.Column<Guid>(nullable: false),
+                    RouteId = table.Column<Guid>(nullable: false),
+                    Distance = table.Column<double>(nullable: false),
+                    Duration = table.Column<TimeSpan>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tickets", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Routes_RouteId",
+                        column: x => x.RouteId,
+                        principalTable: "Routes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Tickets_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Routes_FromId",
+                table: "Routes",
+                column: "FromId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Routes_ToId",
+                table: "Routes",
+                column: "ToId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_RouteId",
+                table: "Tickets",
+                column: "RouteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_UserId",
+                table: "Tickets",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntityAuditLogs");
+
+            migrationBuilder.DropTable(
+                name: "Tickets");
+
+            migrationBuilder.DropTable(
+                name: "Routes");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Stations");
+        }
+    }
+}
