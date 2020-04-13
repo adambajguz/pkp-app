945381af65654aee85dc8755024cd557100347ec
Adam Bajguz <adam.bajguz@gmail.com>
2020-04-13 14:19:26 +0200

    Refactor of AuditableUoW

--- backend/TrainsOnline.Application/Interfaces/UoW/IGenericAuditableUnitOfWork.cs
+++ backend/TrainsOnline.Application/Interfaces/UoW/IGenericAuditableUnitOfWork.cs
@@ -1,9 +1,14 @@
 ﻿namespace TrainsOnline.Application.Interfaces.UoW
 {
+    using System.Threading;
+    using System.Threading.Tasks;
     using Application.Interfaces.Repository;
 
     public interface IGenericAuditableUnitOfWork : IGenericUnitOfWork
     {
         IEntityAuditLogRepository EntityAuditLogRepository { get; }
+
+        int SaveChangesWithoutAudit();
+        Task<int> SaveChangesWithoutAuditAsync(CancellationToken cancellationToken = default);
     }
 }
