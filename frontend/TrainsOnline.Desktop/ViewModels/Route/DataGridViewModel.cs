ac9753b58d625e81ad01457a3e447c15e0ee964d
Adam Bajguz <adam.bajguz@gmail.com>
2020-04-28 19:48:26 +0200

    Refactor

---
+++ frontend/TrainsOnline.Desktop/ViewModels/Route/DataGridViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace TrainsOnline.Desktop.ViewModels.Route
+{
+    using System.Collections.ObjectModel;
+    using System.Threading.Tasks;
+    using GalaSoft.MvvmLight;
+    using TrainsOnline.Desktop.Application.Models;
+    using TrainsOnline.Desktop.Application.Services;
+
+    public class DataGridViewModel : ViewModelBase
+    {
+        public ObservableCollection<SampleOrder> Source { get; } = new ObservableCollection<SampleOrder>();
+
+        public DataGridViewModel()
+        {
+        }
+
+        public async Task LoadDataAsync()
+        {
+            Source.Clear();
+
+            // TODO WTS: Replace this with your actual data
+            System.Collections.Generic.IEnumerable<SampleOrder> data = await SampleDataService.GetGridDataAsync();
+
+            foreach (SampleOrder item in data)
+                Source.Add(item);
+        }
+    }
+}
---
+++ frontend/TrainsOnline.Desktop/Views/Station/MasterDetailDetailControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace TrainsOnline.Desktop.Views.Examples
+{
+    using TrainsOnline.Desktop.Application.Models;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    public sealed partial class MasterDetailDetailControl : UserControl
+    {
+        public SampleOrder MasterMenuItem
+        {
+            get => GetValue(MasterMenuItemProperty) as SampleOrder;
+            set => SetValue(MasterMenuItemProperty, value);
+        }
+
+        public static readonly DependencyProperty MasterMenuItemProperty = DependencyProperty.Register("MasterMenuItem", typeof(SampleOrder), typeof(MasterDetailDetailControl), new PropertyMetadata(null, OnMasterMenuItemPropertyChanged));
+
+        public MasterDetailDetailControl()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnMasterMenuItemPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            MasterDetailDetailControl control = d as MasterDetailDetailControl;
+            control.ForegroundElement.ChangeView(0, 0, 1);
+        }
+    }
+}
