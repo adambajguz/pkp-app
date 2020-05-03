681ca88f66dd646707a99c84c2e6fec3e1682655
Adam Bajguz <adam.bajguz@gmail.com>
2020-05-03 20:13:03 +0200

    HSTS

--- backend/TrainsOnline.Api/Startup.cs
+++ backend/TrainsOnline.Api/Startup.cs
@@ -92,7 +92,7 @@ namespace TrainsOnline.Api
             {
                 app.UseExceptionHandler("/error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-              //  app.UseHsts();
+                app.UseHsts();
             }
 
             app.ConfigureSpecialPages(Environment, _services);
