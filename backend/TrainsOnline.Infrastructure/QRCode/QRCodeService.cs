90d91ed30faa05f4ea70d5f2a9d147a688507fdb
Adam Bajguz <adam.bajguz@gmail.com>
2020-05-03 17:18:23 +0200

    Test

--- backend/TrainsOnline.Infrastructure/QRCode/QRCodeService.cs
+++ backend/TrainsOnline.Infrastructure/QRCode/QRCodeService.cs
@@ -92,7 +92,7 @@
             using (MemoryStream memoryStream = new MemoryStream())
             {
                 bmp = new Bitmap(qrMemoryStream);
-                bmp.Save(memoryStream, ImageFormat.Emf);
+                bmp.Save(memoryStream, ImageFormat.Tiff);
                 return memoryStream.ToArray();
             }
         }
